b1bb57961aa15aa12bfa8c70e3ee0761520741e3
minhhuy356 <huyminh356@gmail.com>
2024-09-01 16:55:23 +0700

    Connect to MongoDB Atlas

---
+++ src/config/mongodb.js
@@ -0,0 +1,44 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+//huyminh356
+//TxQAsM4cwVWbYK1U
+
+const MONGODB_URI =
+  "mongodb+srv://huyminh356:TxQAsM4cwVWbYK1U@camibackend.v6rfg.mongodb.net/?retryWrites=true&w=majority&appName=CamiBackend";
+
+const DATABASE_NAME = "Cami-database";
+
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+//Khởi đầu đối một đối tượng camiDatabaseInstance ban đầu là null (Vì chưa connect tới database)
+let camiDatabaseInstance = null;
+
+//Khởi tạo đối tượng mongoClientInstance để kết nối tới MongoDb
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  // Lưu ý: serverApi có từ phiên bản MongoDB 5.0.0 trở lên, có thể không cần dùng nó, còn nếu dùng nó là chúng ta sẽ chỉ định mọt cái Stable API Version của MongoDB
+  // Đọc thêm tại: http://www.mongodb.com/docs/drivers/node/current/fundamentals/stable-api/
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+//Kết nối tới Database
+export const CONNECT_DB = async () => {
+  //Gọi kết nối tới MongoDB Atlas với URI đã khai báo trong thân của ClientInstance
+  await mongoClientInstance.connect();
+
+  //Kết nối thành công thì lấy ra Database theo tên và gán ngược nó lại vào biến camiDatabaseInstance ở trên của chúng ta
+  camiDatabaseInstance = mongoClientInstance.db(DATABASE_NAME);
+};
+
+//Function GET_DB (không async) này có nhiệm vụ export ra cái Cami Database Instance sau khi đã connect thành công tới MongoDB đẻ chugns ta sử dụng ở nhiều nơi khác nhau trong code
+//Lưu ý phải đảm bảo chỉ luôn gọi cái getDB này sau khi đã kết nối thành công tới MongoDB
+export const GET_DB = () => {
+  if (!camiDatabaseInstance) throw new Error("Must connect to Database first");
+  return camiDatabaseInstance;
+};
---
+++ src/server.js
@@ -0,0 +1,34 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+
+import express from "express";
+import { CONNECT_DB, GET_DB } from "./config/mongodb";
+
+const START_SERVER = () => {
+  const app = express();
+
+  const hostname = "localhost";
+  const port = 8017;
+
+  app.get("/", async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray());
+    // Test Absolute import mapOrder
+    res.end("<h1>Hello World!</h1><hr>");
+  });
+
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello Minh Huy, I am running at http://${hostname}:${port}/`);
+  });
+};
+
+CONNECT_DB()
+  .then(() => console.log("Connect to MongoDB Cloud Atlas!"))
+  .then(() => START_SERVER())
+  .catch((error) => {
+    console.error(error);
+    process.exit(0);
+  });
